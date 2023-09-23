b4dde6aa25196f58da7a797c9819a8a4b71aa7a9
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-09-23 19:01:36 +0600

    Initial Commit

---
+++ WeatherProjectNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherProjectNew
+//
+//  Created by Olzhas Zhakan on 23.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
